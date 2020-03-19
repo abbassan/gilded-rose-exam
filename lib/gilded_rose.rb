fb2ab5454e60d4d43b746ee850a4c646e26238f4
deen1234 <abbasabbasmhamed19@gmail.com>
2020-03-19 15:05:15 +0800

    Added Conjured Mana Cake item in update_item

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -10,46 +10,53 @@ class GildedRose
   end
 
   def update_item(item)
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          decrease_quality(item)
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        decrease_quality(item)
+      end
+      if item.name == "Conjured Mana Cake"
+        decrease_quality(item)
+        decrease_quality(item)
+      end
+      else
+        increase_quality(item)
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in < 11
+          increase_quality(item)
+        end
+        if item.sell_in < 6
+          increase_quality(item)
+        end
+      end
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in -= 1
+    end
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"      
+            if item.name != "Sulfuras, Hand of Ragnaros"
+              decrease_quality(item)
+            end
+            if item.name == "Conjured Mana Cake"
+              decrease_quality(item)
+              decrease_quality(item)
+            end
+        else
+          item.quality = item.quality - item.quality
         end
       else
-          increase_quality(item)
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              increase_quality(item)
-            end
-            if item.sell_in < 6
-              increase_quality(item)
-            end
-          end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                decrease_quality(item)
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          increase_quality(item)
-        end
+        increase_quality(item)
       end
     end
+  end
 
-    def increase_quality(item)
-      item.quality += 1 if item.quality < 50
-    end
+  def increase_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
 
-     def decrease_quality(item)
-      item.quality -= 1 if item.quality > 0
-    end
+  def decrease_quality(item)
+    item.quality -= 1 if item.quality > 0
+  end
 
 end
